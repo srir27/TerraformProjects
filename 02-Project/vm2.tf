37abc81dfb7e8f8d79573fb718fd814e12aacd19
Sri Balaji R <123907132+srir27@users.noreply.github.com>
2025-01-24 08:01:26 +0530

    update: admin credentials vm2.tf

--- 02-Project/vm2.tf
+++ 02-Project/vm2.tf
@@ -18,7 +18,7 @@ resource "azurerm_linux_virtual_machine" "vm2" {
   location = azurerm_resource_group.rg[1].location
   size                = "Standard_B1s"
   admin_username      = "adminuser"
-  admin_password = "Welcome@1234"
+  admin_password = "***********"
   network_interface_ids = [
     azurerm_network_interface.nic2[count.index].id,
   ]
